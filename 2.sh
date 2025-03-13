b092960096a92d00aaccdc5dc7b5c6a6e1750199
Piercingxx <155384428+Piercingxx@users.noreply.github.com>
2025-03-13 11:01:54 -0400

    Update 2.sh

--- 2.sh
+++ 2.sh
@@ -46,7 +46,7 @@ paru -S visual-studio-code-bin --noconfirm
 paru -S mission-center --noconfirm
 paru -S obsidian --noconfirm
 paru -S libreoffice-fresh --noconfirm
-paru -S kdenlive-git --noconfirm
+#paru -S kdenlive-git --noconfirm
 # Gimp and GPU drivers for it
 paru -S gimp-devel --noconfirm
 paru -S opencl-amd --noconfirm
