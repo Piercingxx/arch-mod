861c3927506378287e2cf30437c604713b7f76d7
Piercingxx <155384428+Piercingxx@users.noreply.github.com>
2025-03-13 10:40:26 -0400

    Update 1.sh

--- 1.sh
+++ 1.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-sudo pacman -Syu
-sudo pacman -S zip unzip gzip tar make --noconfirm 
+# Clone and install Paru
+git clone https://aur.archlinux.org/paru-bin.git && cd paru-bin && makepkg -si --noconfirm && cd ..
 
+sudo pacman -Syu
+sudo pacman -S zip unzip gzip tar make --noconfirm
 
 # Enable Bluetooth
 sudo systemctl start bluetooth
@@ -13,18 +15,9 @@ sudo pacman -S cups gutenprint cups-pdf gtk3-print-backends nmap net-tools cmake
 systemctl enable cups.service
 systemctl start cups
 
-
-# Update system and install Git
-sudo pacman -Syu git --noconfirm
-
-# Clone and install Paru
-git clone https://aur.archlinux.org/paru-bin.git && cd paru-bin && makepkg -si --noconfirm && cd ..
-
 #Add Flatpak
 sudo pacman -S flatpak --noconfirm
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
-
-
 
 #Synology Drive doesnt support wayland so run this..
 QT_QPA_PLATFORM=xcb
