4a156875e13ab3239722293944effbd2892f0cf3
Piercingxx <155384428+Piercingxx@users.noreply.github.com>
2025-03-13 12:28:00 -0400

    Update 1.sh

--- 1.sh
+++ 1.sh
@@ -1,16 +1,22 @@
 #!/bin/bash
 
+#!/bin/bash
+
 # Check if running as root. If root, script will exit
 if [[ $EUID -eq 0 ]]; then
     echo "This script should not be executed as root! Exiting......."
     exit 1
 fi
 
+sudo pacman -Syu
+sudo pacman -S zip unzip gzip tar make --noconfirm
+
 # Clone and install Paru
 git clone https://aur.archlinux.org/paru-bin.git && cd paru-bin && makepkg -si --noconfirm && cd ..
 
-sudo pacman -Syu
-sudo pacman -S zip unzip gzip tar make --noconfirm
+#Add Flatpak
+sudo pacman -S flatpak --noconfirm
+flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 
 # Enable Bluetooth
 sudo systemctl start bluetooth
@@ -20,10 +26,6 @@ systemctl enable bluetooth
 sudo pacman -S cups gutenprint cups-pdf gtk3-print-backends nmap net-tools cmake meson cpio --noconfirm
 systemctl enable cups.service
 systemctl start cups
-
-#Add Flatpak
-sudo pacman -S flatpak --noconfirm
-flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 
 #Synology Drive doesnt support wayland so run this..
 QT_QPA_PLATFORM=xcb
