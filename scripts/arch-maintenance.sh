7a9a50f784120a26977ddac44b389a91dc786235
Piercingxx <155384428+Piercingxx@users.noreply.github.com>
2025-05-02 13:51:11 -0400

    update

--- scripts/arch-maintenance.sh
+++ scripts/arch-maintenance.sh
@@ -8,14 +8,6 @@ command_exists() {
     command -v "$1" >/dev/null 2>&1
 }
 
-# Function to cache sudo credentials
-cache_sudo_credentials() {
-    echo "Caching sudo credentials for script execution..."
-    sudo -v
-    # Keep sudo credentials fresh for the duration of the script
-    (while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &)
-}
-
 # Checks for active network connection
 if [[ -n "$(command -v nmcli)" && "$(nmcli -t -f STATE g)" != connected ]]; then
     awk '{print}' <<<"Network connectivity is required to continue."
@@ -31,9 +23,6 @@ fi
 username=$(id -u -n 1000)
 builddir=$(pwd)
 
-# Cache sudo credentials
-cache_sudo_credentials
-
 # Function to display a message box
 function msg_box() {
     whiptail --msgbox "$1" 0 0 0
@@ -43,16 +32,10 @@ function msg_box() {
 function menu() {
     whiptail --backtitle "GitHub.com/PiercingXX" --title "Main Menu" \
         --menu "Run Options In Order:" 0 0 0 \
-        "Update Mirrors"                        "Update Mirrors & adds "arch-maintenance.sh" to /home" \
+        "Update Mirrors"                        "Update Mirrors" \
         "Update System"                         "Update System" \
-        "Add Paru, Flatpak, & Dependencies"     "Will Automatically Reboot After" \
-        "Applications"                          "Install Applications and Utilities" \
-        "Hyprland"                              "This Will Install Hyprland & All Dependencies" \
-        "Gnome Extensions"                      "My Favorite Gnome Shell Extensions" \
         "Piercing Gimp"                         "Piercing Gimp Presets (Distro Agnostic)" \
         "PiercingXX Rice"                       "Apply Piercing Rice (Distro Agnostic)" \
-        "Beautiful Bash"                        "Chris Titus' Beautiful Bash Script" \
-        "Surface Kernel"                        "Install Surface Kernal" \
         "Reboot System"                         "Reboot the system" \
         "Exit"                                  "Exit the script" 3>&1 1>&2 2>&3
 }
@@ -82,12 +65,6 @@ while true; do
             # Update mirrors
             sudo -v reflector --verbose --sort rate -l 75 --save /etc/pacman.d/mirrorlist
             echo -e "${GREEN}Mirrors Updated${NC}"
-            # Copy arch-maintenance.sh to home directory
-                cd script || exit
-                cp -f arch-maintenance.sh /home/"$username"
-                chown "$username":"$username" /home/"$username"/arch-maintenance.sh
-                cd "$builddir" || exit
-            echo -e "${GREEN}Arch-Update.sh Copied To Home Directory${NC}"
             ;;
         "Update System")
             echo -e "${YELLOW}Updating System...${NC}"
@@ -118,58 +95,6 @@ while true; do
                 fi
             echo -e "${GREEN}System Updated Successfully!${NC}"
             ;;
-        "Add Paru, Flatpak, & Dependencies"*)
-            echo -e "${YELLOW}Installing Paru, Flatpak, & Dependencies...${NC}"
-            # Install dependencies
-            echo "# Installing dependencies..."
-            sudo pacman -S zip unzip gzip tar make --noconfirm
-            # Clone and install Paru
-            echo "# Cloning and installing Paru..."
-            git clone https://aur.archlinux.org/paru-bin.git && cd paru-bin && makepkg -si --noconfirm && cd ..
-            # Add Flatpak
-            echo "# Installing Flatpak..."
-            sudo -v pacman -S flatpak --noconfirm
-            flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
-            echo "# Enabling Bluetooth and Printer services..."
-            # Enable Bluetooth
-            sudo -v systemctl start bluetooth
-            systemctl enable bluetooth
-            # Enable Printer 
-            sudo -v pacman -S cups gutenprint cups-pdf gtk3-print-backends nmap net-tools cmake meson cpio --noconfirm
-            sudo -v systemctl enable cups.service
-            sudo -v systemctl start cups
-            msg_box "System will reboot now. Re-run the script after reboot to continue."
-            sudo reboot
-            ;;
-        "Hyprland"*)
-            echo -e "${YELLOW}Installing Hyprland & Dependencies...${NC}"
-                cd scripts || exit
-                chmod u+x hyprland-install.sh
-                ./hyprland-install.sh
-                cd "$builddir" || exit
-            echo -e "${GREEN}Installed successfully!${NC}"
-            ;;     
-        "Applications")
-            echo -e "${YELLOW}Installing Core Applications...${NC}"
-                cd scripts || exit
-                chmod u+x apps.sh
-                ./apps.sh
-                cd "$builddir" || exit
-            echo -e "${GREEN}Core Apps Installed successfully!${NC}"
-            ;;
-        "Gnome Extensions"*)
-            echo -e "${YELLOW}Installing Gnome Extensions...${NC}"
-                paru -S gnome-shell-extension-appindicator-git --noconfirm
-                paru -S gnome-shell-extension-app-icons-taskbar --noconfirm
-                paru -S gnome-shell-extension-blur-my-shell-git --noconfirm
-                paru -S gnome-shell-extension-just-perfection-desktop --noconfirm
-                paru -S gnome-shell-extension-pop-shell-git --noconfirm
-                paru -S gnome-shell-extension-space-bar-git --noconfirm
-                paru -S gnome-shell-extension-useless-gaps-git --noconfirm
-                paru -S gnome-shell-extension-caffeine-git --noconfirm
-                paru -S gnome-shell-extension-gsconnect --noconfirm
-            echo -e "${GREEN}Gnome Extensions Installed successfully!${NC}"
-            ;;   
         "Piercing Gimp")
             # Gimp Dots
                 echo -e "${YELLOW}Installing Piercing Gimp Presets...${NC}"
@@ -210,45 +135,6 @@ while true; do
                 # Add in backgrounds and themes and apply them
                 rm -rf piercing-dots
             echo -e "${GREEN}PiercingXX Rice Applied Successfully!${NC}"
-            ;;
-        "Beautiful Bash")
-            echo -e "${YELLOW}Installing Beautiful Bash...${NC}"
-                git clone https://github.com/christitustech/mybash
-                    chmod -R u+x mybash
-                    chown -R "$username":"$username" mybash
-                    cd mybash || exit
-                    ./setup.sh
-                    cd "$builddir" || exit
-                    rm -rf mybash
-            ;;
-        "Surface Kernel")
-            echo -e "${YELLOW}Microsoft Surface Kernel...${NC}"            
-                curl -s https://raw.githubusercontent.com/linux-surface/linux-surface/master/pkg/keys/surface.asc \
-                    | sudo pacman-key --add -
-                sudo pacman-key --finger 56C464BAAC421453
-                sudo pacman-key --lsign-key 56C464BAAC421453
-                    if [ -f "/etc/pacman.conf" ]; then
-                        cat /etc/pacman.conf >> /etc/pacman.conf
-                        echo "Server = https://pkg.surfacelinux.com/arch/" >> /etc/pacman.conf
-                    else
-                        echo "The file does not exist."
-                    fi
-                paru -Syu --noconfirm
-                paru -Sy libwacom-surface --noconfirm
-                sudo pacman -Syu --noconfirm
-                sudo pacman -S linux-surface linux-surface-headers iptsd --noconfirm
-                sudo pacman -S linux-firmware-marvell --noconfirm
-                sudo pacman -S linux-surface-secureboot-mok --noconfirm
-                paru -S surface-dtx-daemon --noconfirm
-                systemctl enable surface-dtx-daemon.service
-                systemctl --user surface-dtx-userd.service
-                sudo touch /boot/loader/entries/surface.conf
-                    cat /boot/loader/entries/surface.conf >> /boot/loader/entries/surface.conf
-                    echo "
-                    title Arch Surface
-                    linux /vmlinuz-linux-surface
-                    initrd  /initramfs-linux-surface.img
-                    options root=LABEL=arch rw"
             ;;
         "Reboot System")
             echo -e "${YELLOW}Rebooting system in 3 seconds...${NC}"
