2f4facd1f5ccc46f7c93dc440a5159688b20c849
Piercingxx <155384428+Piercingxx@users.noreply.github.com>
2025-10-30 21:08:35 -0400

    Fix typo in package installation command

--- scripts/step-1.sh
+++ scripts/step-1.sh
@@ -60,6 +60,7 @@ builddir=$(pwd)
         echo "# Installing more dependencies..."
         paru -S dconf --noconfirm
         paru -S cpio cmake meson --nocofirm
+        paru -S libinput-gestures --noconfirm
     # Yazi
         paru -S yazi-nightly-bin --noconfirm
         paru -S ffmpeg --noconfirm
